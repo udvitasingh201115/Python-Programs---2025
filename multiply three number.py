1b910bcdba5edbdde99a2eaa7f24d9612eb55d7c
udvitasingh201115 <udvitasingh2011@gmail.com>
2025-11-07 18:55:34 +0530

    Add files via upload

---
+++ multiply three number.py
@@ -0,0 +1,7 @@
+#wap to multiply three number
+number1=int(input("enter first number"))
+number2=int(input("enter second number"))
+number3=int(input("enter third number"))
+result=(number1*number2*number3)
+print(  "the result is ",result)
+
