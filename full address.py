f03b0dfce27e37db34f13c533a23ba18cf6869c8
udvitasingh201115 <udvitasingh2011@gmail.com>
2025-11-12 19:10:11 +0530

    Add files via upload

---
+++ full address.py
@@ -0,0 +1,11 @@
+#WAP to take street name , locality , area city , pin number and display the full addresss
+name = input("enter name")
+pin =input("enter pin number")
+locality = input("enter locality")
+city = input("enter city")
+state = input("enter state")
+country = input("enter country")
+#continent = input("enter continent")
+
+print ("Hi!", name ,  "your address is", pin, "," ,locality, ",", city, "," , state, "," , country, )R
+
